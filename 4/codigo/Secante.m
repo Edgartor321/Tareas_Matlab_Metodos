bd03b89d335664054fe2bf521abb5f080b292052
samzhand <edgar18tor@gmail.com>
2025-11-28 18:49:56 -0600

    init-carpertas-y-primeras-lineas-de-la-tarea-4

---
+++ 4/codigo/Secante.m
@@ -0,0 +1,13 @@
+function [salEva,salPE] = Secante(funcion,a,b,PE)
+%Función para realizar método de la secante
+%salEva=xn ó raíz de la función
+%salPE= Error porcentual de la raíz
+%---------------------------------------
+%funcion=función a evaluar 
+%a: punto a
+%b: punto b
+%PE: Erorr porcentual deseado
+feval()
+
+
+end
